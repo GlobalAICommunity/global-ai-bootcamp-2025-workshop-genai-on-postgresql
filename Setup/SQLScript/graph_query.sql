bb00c8f21b010b85b4956342b4b43f792a2b6d9c
Abraham Omorogbe <9068012+AbeOmor@users.noreply.github.com>
2024-11-08 18:26:44 -0800

    Added Graph RAG

--- Setup/SQLScript/graph_query.sql
+++ Setup/SQLScript/graph_query.sql
@@ -1,89 +1,86 @@
-CREATE EXTENSION IF NOT EXISTS age CASCADE;
+CREATE OR REPLACE FUNCTION semantic_relevance(query TEXT, n INT)
+RETURNS jsonb AS $$
+DECLARE
+    json_pairs jsonb;
+	result_json jsonb;
+BEGIN
+	json_pairs := generate_json_pairs(query, n);
+	result_json := azure_ml.invoke(
+				json_pairs,
+				deployment_name=>'bge-v2-m3-1',
+				timeout_ms => 180000);
+	RETURN (
+		SELECT result_json as result
+	);
+END $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generate_json_pairs(query TEXT, n INT)
+RETURNS jsonb AS $$
+BEGIN
+    RETURN (
+        SELECT jsonb_build_object(
+            'pairs', 
+            jsonb_agg(
+                jsonb_build_array(query, LEFT(text, 800))
+            )
+        ) AS result_json
+        FROM (
+            SELECT id, opinion AS text
+		    FROM cases
+		    ORDER BY opinions_vector <=> azure_openai.create_embeddings('text-embedding-3-small', query)::vector
+		    LIMIT n
+        ) subquery
+    );
+END $$ LANGUAGE plpgsql;
 
 LOAD 'age';
 SET search_path = public, ag_catalog, "$user";
 
 WITH
-user_query as (
-	select 'Water leaking into the apartment from the floor above.' as query_text
-),
 embedding_query AS (
-    SELECT query_text, azure_openai.create_embeddings('text-embedding-3-small', query_text)::vector AS embedding
-    from user_query
+    SELECT azure_openai.create_embeddings('text-embedding-3-small', 'Water leaking into the apartment from the floor above.')::vector AS embedding
 ),
 vector AS (
-    SELECT cases.id, cases.name AS case_name, cases.decision_date AS date, cases.opinion AS opinion,
-    RANK() OVER (ORDER BY opinions_vector <=> embedding) AS vector_rank, query_text, embedding
+    SELECT cases.id, cases.name AS case_name, cases.decision_date AS date, cases.opinion, RANK() OVER (ORDER BY opinions_vector <=> embedding) AS vector_rank
     FROM cases, embedding_query
     ORDER BY opinions_vector <=> embedding
     LIMIT 60
 ),
-json_payload AS (
-    SELECT jsonb_build_object(
-        'pairs', 
-        jsonb_agg(
-            jsonb_build_array(
-                query_text, 
-                LEFT(vector.opinion, 8000)
-            )
-        )
-    ) AS json_pairs
-    FROM vector
-),
 semantic AS (
-    SELECT elem.relevance::DOUBLE precision as relevance, elem.ordinality
-    FROM json_payload,
-         LATERAL jsonb_array_elements(
-             azure_ml.invoke(
-                 json_pairs,
-                 deployment_name => 'bge-v2-m3-1',
-                 timeout_ms => 180000
-             )
-         ) WITH ORDINALITY AS elem(relevance)
+    SELECT * 
+    FROM jsonb_array_elements(
+            semantic_relevance('Water leaking into the apartment from the floor above.',
+            60)
+        ) WITH ORDINALITY AS elem(relevance)
 ),
 semantic_ranked AS (
-    SELECT RANK() OVER (ORDER BY relevance DESC) AS semantic_rank,
+    SELECT semantic.relevance::DOUBLE PRECISION AS relevance, RANK() OVER (ORDER BY relevance DESC) AS semantic_rank,
 			semantic.*, vector.*
     FROM vector
     JOIN semantic ON vector.vector_rank = semantic.ordinality
     ORDER BY semantic.relevance DESC
 ),
 graph AS (
-	select id, COUNT(ref_id) AS refs
-	from (
-	    SELECT semantic_ranked.id, graph_query.ref_id, c2.opinions_vector <=> embedding AS ref_cosine
-		FROM semantic_ranked
-		LEFT JOIN cypher('case_graph', $$
-	            MATCH (s)-[r:REF]->(n)
-	            RETURN n.case_id AS case_id, s.case_id AS ref_id
-	        $$) as graph_query(case_id TEXT, ref_id TEXT)
-		ON semantic_ranked.id = graph_query.case_id::int
-		LEFT JOIN cases c2
-		ON c2.id = graph_query.ref_id::int
-		WHERE semantic_ranked.semantic_rank <= 25
-		ORDER BY ref_cosine
-		LIMIT 200
-	)
-	group by id
-),
-graph2 as (
-	select semantic_ranked.*, graph.refs 
-	from semantic_ranked
-	left join graph
-	on semantic_ranked.id = graph.id::int
+    SELECT graph_query.refs, semantic_ranked.vector_rank, semantic_ranked.*, graph_query.case_id from semantic_ranked
+	LEFT JOIN cypher('case_graph', $$
+            MATCH ()-[r]->(n)
+            RETURN n.case_id, COUNT(r) AS refs
+        $$) as graph_query(case_id TEXT, refs BIGINT)
+	ON semantic_ranked.id = graph_query.case_id::int
 ),
 graph_ranked AS (
-    SELECT RANK() OVER (ORDER BY COALESCE(graph2.refs, 0) DESC) AS graph_rank, graph2.*
-    FROM graph2 ORDER BY graph_rank DESC
+    SELECT RANK() OVER (ORDER BY COALESCE(graph.refs, 0) DESC) AS graph_rank, graph.*
+    FROM graph ORDER BY graph_rank DESC
 ),
 rrf AS (
-    select
+    SELECT
         COALESCE(1.0 / (60 + graph_ranked.graph_rank), 0.0) +
         COALESCE(1.0 / (60 + graph_ranked.semantic_rank), 0.0) AS score,
         graph_ranked.*
     FROM graph_ranked
     ORDER BY score DESC
+    LIMIT 20
 )
-select id,case_name
+SELECT id, case_name, opinion 
 FROM rrf
-order by score DESC;
+LIMIT 10;
---
+++ Setup/SQLScript/graph_setup.sql
@@ -0,0 +1,103 @@
+-- Create a temp table to store the cases data
+DROP TABLE IF EXISTS temp_cases;
+CREATE TABLE temp_cases(data jsonb);
+\COPY temp_cases (data) FROM '/mslearn-pg-ai/Setup/Data/cases.csv' WITH (FORMAT csv, HEADER true);
+
+CREATE OR REPLACE FUNCTION create_case_in_case_graph(case_id text)
+RETURNS void
+LANGUAGE plpgsql
+VOLATILE
+AS $BODY$
+BEGIN
+	load 'age';
+	SET search_path TO ag_catalog;
+	EXECUTE format('SELECT * FROM cypher(''case_graph'', $$CREATE (:case {case_id: %s})$$) AS (a agtype);', quote_ident(case_id));
+END
+$BODY$;
+
+CREATE OR REPLACE FUNCTION create_case_link_in_case_graph(id_from int4, id_to int4)
+RETURNS void
+LANGUAGE plpgsql
+VOLATILE
+AS $BODY$
+BEGIN
+	load 'age';
+	SET search_path TO ag_catalog;
+	EXECUTE format('SELECT * FROM cypher(''case_graph'', $$MATCH (a:case), (b:case) WHERE a.case_id = %s AND b.case_id = %s CREATE (a)-[e:REF]->(b) RETURN e$$) AS (a agtype);', quote_ident(id_from), quote_ident(id_to));
+END
+$BODY$;
+
+
+
+CREATE OR REPLACE FUNCTION create_case(case_id text)
+RETURNS void
+LANGUAGE plpgsql
+VOLATILE
+AS $BODY$
+BEGIN
+	load 'age';
+	SET search_path TO ag_catalog;
+	EXECUTE format('SELECT * FROM cypher(''case_graph_full'', $$CREATE (:case {case_id: %s})$$) AS (a agtype);', quote_ident(case_id));
+END
+$BODY$;
+
+CREATE OR REPLACE FUNCTION create_case_link(id_from text, id_to text)
+RETURNS void
+LANGUAGE plpgsql
+VOLATILE
+AS $BODY$
+BEGIN
+	load 'age';
+	SET search_path TO ag_catalog;
+	EXECUTE format('SELECT * FROM cypher(''case_graph_full'', $$MATCH (a:case), (b:case) WHERE a.case_id = %s AND b.case_id = %s CREATE (a)-[e:REF]->(b) RETURN e$$) AS (a agtype);', quote_ident(id_from), quote_ident(id_to));
+END
+$BODY$;
+
+-- CREATION of case_graph
+SELECT * FROM ag_catalog.drop_graph('case_graph', true);
+LOAD 'age';
+SET search_path = public, ag_catalog, "$user";
+
+SELECT create_graph('case_graph');
+
+-- Create nodes (doesn't work in dbeaver, but works in pgadmin)
+SELECT create_case_in_case_graph((temp_cases.data#>>'{id}')::text) 
+FROM public.temp_cases;
+
+SELECT * from cypher('case_graph', $$
+                    MATCH (n)
+                    RETURN COUNT(n.case_id)
+                $$) as (case_id TEXT);
+
+WITH edges AS (
+	SELECT c1.data#>>'{id}' AS id_from, c2.data#>>'{id}' AS id_to
+	FROM public.temp_cases c1
+	LEFT JOIN 
+	    LATERAL jsonb_array_elements(c1.data -> 'cites_to') AS cites_to_element ON true
+	LEFT JOIN 
+	    LATERAL jsonb_array_elements(cites_to_element -> 'case_ids') AS case_ids ON true
+	JOIN public.temp_cases c2 
+		ON case_ids::text = c2.data#>>'{id}'
+)
+SELECT public.create_case_link_in_case_graph(edges.id_from::text, edges.id_to::text) 
+FROM edges
+limit 1;
+
+WITH edges AS (
+	SELECT DISTINCT c1.data#>>'{id}' AS id_from, c2.data#>>'{id}' AS id_to
+	FROM public.temp_cases c1
+	LEFT JOIN 
+	    LATERAL jsonb_array_elements(c1.data -> 'cites_to') AS cites_to_element ON true
+	LEFT JOIN 
+	    LATERAL jsonb_array_elements(cites_to_element -> 'case_ids') AS case_ids ON true
+	JOIN public.temp_cases c2 
+		ON case_ids::text = c2.data#>>'{id}'
+), gedges AS (
+	SELECT edges.id_from, node1.id AS gid_from, edges.id_to, node2.id AS gid_to
+	FROM edges
+	LEFT JOIN case_graph."case" node1 ON node1.properties::json ->> 'case_id' = edges.id_from
+	LEFT JOIN case_graph."case" node2 ON node2.properties::json ->> 'case_id' = edges.id_to
+)
+INSERT INTO case_graph."REF" (start_id, end_id)
+SELECT gid_from AS start_id, gid_to AS end_id
+FROM gedges;
